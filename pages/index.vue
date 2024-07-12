d6e7fd34b41f3d424015c23a08a3905c063a6334
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2024-07-12 13:35:10 +0200

    feat: add title card

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="grid grid-cols-6 gap-4">
-    <div v-for="i in 10" :key="i" class="card">
-      <span class="text-lg">Nom magasin</span>
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis
-        exercitationem nemo? Exercitationem, magni perferendis ut, amet rerum
-        natus ex enim eveniet id magnam atque tenetur repudiandae tempore?
-        Consequuntur, a?
-      </span>
+  <div class="flex flex-col space-y-6">
+    <div class="card">
+      <span class="text-xl font-semibold">Liste des magasins</span>
+    </div>
+
+    <div class="grid grid-cols-6 gap-4">
+      <div v-for="i in 10" :key="i" class="card">
+        <span class="text-lg">Nom magasin</span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis
+          exercitationem nemo? Exercitationem, magni perferendis ut, amet rerum
+          natus ex enim eveniet id magnam atque tenetur repudiandae tempore?
+          Consequuntur, a?
+        </span>
+      </div>
     </div>
   </div>
 </template>
